0fb4131d5a175e80bd1a793c81482ef00f87df21
Gabrielbbe <104850235+Gabrielbbe@users.noreply.github.com>
2023-01-21 13:37:44 -0300

    Update streamlit_part.py

--- Amazon_webscraping_dashboard/Deploy/streamlit_part.py
+++ Amazon_webscraping_dashboard/Deploy/streamlit_part.py
@@ -24,7 +24,7 @@ df_pais_marca, df = get_data()
 
 st.sidebar.markdown(' ## Dashboard dos dados dos celulares mais vendidos no site da Amazon brasil, o ranking vai do 1º celular mais vendido para o menos vendido, \
 os dados foram coletados dia 20/01/2023.  \n')
-st.sidebar.markdown('  \n')
+st.sidebar.text(' ')
 st.sidebar.markdown(' ***Se você quiser rodar o script para coletar os dados e gerar um dashboard igual ao que você está vendo na página*** com os \
 dados de hoje ***de forma automatizada*** você pode seguir as ***instruções da pagina do projeto no github:*** https://shre.ink/1WtH  \n Se por algum motivo quiser entrar em ***contato*** comigo ou tiver alguma sugestão ***pode me enviar um e-mail:*** gabbe2.718@gmail.com ')
 
